f239ec37f1d7f4dd7835a09f2cef306c90c8b6de
Chinmayap065 <2023cs_chinmayaputtaswamy_a@nie.ac.in>
2025-09-17 11:41:00 +0530

    pdtcont

--- src/main/java/com/mtd/EcomApp/controller/ProductController.java
+++ src/main/java/com/mtd/EcomApp/controller/ProductController.java
@@ -1,14 +1,7 @@
 package com.mtd.EcomApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import com.mtd.EcomApp.entity.Product;
 import com.mtd.EcomApp.service.ProductService;
@@ -17,31 +10,32 @@ import com.mtd.EcomApp.service.ProductService;
 @RequestMapping("/products")
 @CrossOrigin("*")
 public class ProductController {
-	@Autowired
-	private ProductService productService;
-	@PostMapping("/save")
-	public Product save(@RequestBody Product product) {
-		return productService.saveProduct(product);
-	}
-	@GetMapping("/{id}")
-	public Product findById(String id) {
-		return productService.getProductbyId(id);
-	}
-	
-	@GetMapping("/all")
-	public List<Product> findAll(){
-		return productService.getProducts();
-	}
-	@PutMapping("/{id}")
-	public Product updateProduct(Product product,String id) {
-		return productService.updateProduct(product, id);
-	}
-	@DeleteMapping("/{id}")
-	public boolean deleteProduct(String id) {
-		return productService.deleteProduct(id);
-	}
-	
+    
+    @Autowired
+    private ProductService productService;
 
-	
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
 
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable String id) {
+        return productService.getProductbyId(id);
+    }
+
+    @GetMapping("/all")
+    public List<Product> findAll() {
+        return productService.getProducts();
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@RequestBody Product product, @PathVariable String id) {
+        return productService.updateProduct(product, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteProduct(@PathVariable String id) {
+        return productService.deleteProduct(id);
+    }
 }
